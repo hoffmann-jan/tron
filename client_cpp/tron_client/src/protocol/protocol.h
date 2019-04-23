1d6313fbc4e221c976815d7d514dcacc5255a184
jsmolka <jsmolka@pm.me>
2019-04-23 10:08:45 +0200

    rework protocol

--- client_cpp/tron_client/src/protocol/protocol.h
+++ client_cpp/tron_client/src/protocol/protocol.h
@@ -1,23 +1,40 @@
 #pragma once
 
+#include <string>
 #include <vector>
 
 struct Position
 {
-    int id;  // Player id
-    int x;   // Player x coordinate
-    int y;   // Player y coordinate
+    int x;         // Player x coordinate
+    int y;         // Player y coordinate
+    bool jumping;  // Player jumping status
 };
 
-enum Type  // Type of request / response
+// 1. TYPE_CONNECT: C -> S: (name, lobby id)
+// 2. TYPE_CONNECT: S -> C: (player id, color)
+// 3. TYPE_LOBBY: S -> C: inital lobby message (waiting players and their positions)
+// 3. or TYPE_ADD for waiting players: add new player (new player and position)
+// 4. TYPE_READY: C -> S
+// 5. TYPE_START: S -> C: clients start moving (initially to the middle)
+// 5. TYPE_UPDATE: S -> C: updated positions
+// 5. TYPE_ACTION: C -> S: Action (also sends player id)
+// 5. TYPE_DEAD: S -> C
+// 5. TYPE_DISCONNECT: C -> S, S -> C (sends player id)
+// 6. TYPE_RESULT: S -> C: sends winning player id
+// 7. TYPE_LOBBY: S -> C: Back to 3.
+
+enum Type
 {
-    TYPE_ADD    = 0,  // S  -> C: add a new player to the lobby
-    TYPE_REMOVE = 1,  // S  -> C: remove a player from a lobby / game
-    TYPE_READY  = 2,  // S <-  C: clients can ready up to start the game
-    TYPE_UPDATE = 3,  // S  -> C: server updates player positions
-    TYPE_ACTION = 4,  // S <-  C: the client changes its direction
-    TYPE_DIED   = 5,  // S  -> C: broadcast the death of a player
-    TYPE_LOBBY  = 6   // S  -> C: tells clients to enter the lobby screen
+    TYPE_CONNECT    = 0,  // S <-> C: assigns id to a player
+    TYPE_DISCONNECT = 1,  // S  -> C: removes a player from a lobby / game
+    TYPE_ADD        = 2,  // S  -> C: broadcasts a new player
+    TYPE_UPDATE     = 3,  // S  -> C: server updates player positions
+    TYPE_ACTION     = 4,  // S <-  C: client does something
+    TYPE_DEAD       = 5,  // S  -> C: broadcast the death of a player
+    TYPE_LOBBY      = 6,  // S  -> C: initial message to enter a lobby
+    TYPE_READY      = 7,  // S <-  C: clients can ready up in the lobby
+    TYPE_START      = 8,  // S  -> C: ends the lobby and starts the game
+    TYPE_RESULT     = 9   // S  -> C: shows the end result
 };
 
 enum Action
@@ -29,24 +46,21 @@ enum Action
     ACT_JUMP  = 4
 };
 
+struct Player
+{
+    int id;
+    std::string name;
+    int color;
+    Position position;
+};
+
 struct Protocol
 {
     Type type;
-
     Action action;
 
-    int id;  // Player id, send in TYPE_ADD / TYPE_REMOVE / TYPE_DEAD
+    int lobbyId;
+    int length;
 
-    std::vector<Position> positions;  // Array of player positions
+    std::vector<Player> players;
 };
-
-// Example
-// - C1 connects to the S
-// - S sends C1 its id (TYPE_ADD) and positions it somewhere in the field (TYPE_UPDATE)
-// - S and C1 wait until a second player connects
-// - S adds the seconds player (TYPE_ADD) and positions him on the field (TYPE_UPDATE)
-// - if the lobby contains at least 2 players, they can ready up to start the game (TYPE_READY)
-// - if all players readied up, the game starts and their initial direction is either up or down
-// - players can change their direction or jump and send TYPE_ACTION and ACT_
-// - if a player dies, the server broadcasts it to all other players (TYPE_DIED) and ignores actions of the dead player
-// - after a player won, all other players enter the lobby screen again
